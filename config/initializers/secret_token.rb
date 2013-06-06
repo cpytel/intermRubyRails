e3df18a072cb04d0a27a7db7db0de8f11cca205b
Flo Jones <flocela@gmail.com>
2013-06-06 10:23:12 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = '5099e792bde7f6956b4d45c02030ba46248c1bece153b253b554677dcc5397638e7e7c255df310286200df38af8a646a22a61c058ad6b5ace38584e89165c725'
